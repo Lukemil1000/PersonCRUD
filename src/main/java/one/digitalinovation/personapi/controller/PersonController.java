58546c616a0acbb45a847778721a4322f7e40631
Lucas Detogni Pomar <lucaspomar@outlook.com>
2021-07-24 17:42:15 -0300

    CRUD

---
+++ src/main/java/one/digitalinovation/personapi/controller/PersonController.java
@@ -0,0 +1,52 @@
+package one.digitalinovation.personapi.controller;
+
+import one.digitalinovation.personapi.dto.PersonDTO;
+import one.digitalinovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinovation.personapi.entity.Person;
+import one.digitalinovation.personapi.exception.PersonNotFoundException;
+import one.digitalinovation.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    private PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+        return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
+}
